fc6a74086009e316ab643ad32be7684464d6cc57
Spieny <36728571+Spieny@users.noreply.github.com>
2022-02-20 14:51:33 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -450,10 +450,10 @@ function backBtn() {
 function shareText(score) {
 
     deviation_time = (date2.getTime() - _date1.getTime())
-    if (score <= 2.5 * __Time) return '加油！我相信您可以的！';
-    if (score <= 5 * __Time) return '^_^ 加把劲，底力大王就是您！';
-    if (score <= 7.5 * __Time) return '您！';
-    if (score <= 10 * __Time) return '太 您 了！';
+    if (score <= 2.5 * __Time) return '拜托，你很逊欸！';
+    if (score <= 5 * __Time) return '是个屑东西';
+    if (score <= 7.5 * __Time) return '哎哟，很勇♂哦';
+    if (score <= 10 * __Time) return '太 屑 了！';
     return '您是外星人嘛？';
 }
 
